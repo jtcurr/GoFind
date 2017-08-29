be9852f13f8c4c2bc153de4d27f372b7397679ef
John Currie <johntcurrie@gmail.com>
2017-08-29 09:19:58 -0700

    Added a file with the base64 dress image

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
+import ListComponent from './src/components/ListComponent.js';
 
 export default class App extends React.Component {
 
   render() {
     return (
       <View style={styles.container}>
-      <Text>Hello</Text>
+        <ListComponent />
       </View>
     );
   }
--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Text, View, TouchableHighlight, StyleSheet } from 'react-native';
-import base64Img from 'base64Img';
+import dress from '../../dress';
 
-export class ListComponent extends React.Component {
+export default class ListComponent extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			apiData: 'treats'
+		}
+	}
 
 	componentDidMount() {
+		
+	}
 
-		base64Img.base64('../dress.jpg', function(err, data) {
-			if(err) {
-				console.log('Error retrieving imgage', err);
-				}
-			}
-			else {
-				console.log('Data successfully found', data);
-				const config = {
-					method: 'POST',
-					send: JSON.stringify(data)
-				}
-			}
-		});
+	render() {
+		return (
+			<Text>{ this.state.apiData }</Text>
+		)
 	}
 }
