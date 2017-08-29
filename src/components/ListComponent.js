4ad0c3871c50240312152402311a4b1b7f0251c8
John Currie <johntcurrie@gmail.com>
2017-08-29 16:23:12 -0700

    Mocked out data for data returning from API

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -1,23 +1,50 @@
 import React from 'react';
 import { Text, View, TouchableHighlight, StyleSheet } from 'react-native';
 import dress from '../../dress';
+const axios = require('axios');
 
 export default class ListComponent extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			apiData: 'treats'
+			apiData: []
 		}
 	}
 
-	componentDidMount() {
-		
+	componentWillMount() {
+		const send = {img64: dress.data};
+		const api_address = "https://8n78hbwks0.execute-api.us-west-2.amazonaws.com/dev/";
+		axios.post(api_address, {
+			body: JSON.stringify(send),
+			headers: {"Content-Type":"application/json"} 
+		}).then((response)=> {
+			this.setState({
+				apiData: response
+			})
+			console.log('response', response)
+		}).catch((error)=> {
+			console.log('error', error)
+		})
 	}
 
 	render() {
-		return (
-			<Text>{ this.state.apiData }</Text>
-		)
+		let listItems = this.state.apiData.map(function(item, key) {
+			return (
+				<TouchableHighlight key={key} style={styles.row} onPress={() => thisHolder._onButtonClick(item.data)}>
+					<Text>{ item.data.title }</Text>
+				</TouchableHighlight>
+			);
+		});
+		if(this.state.apiData.length > 0) {
+			return(
+				<View>{ listItems }</View>
+			)
+		}
+		else {
+			return (
+				<Text>Loading data...</Text>
+			)
+		}
 	}
 }
